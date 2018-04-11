d63144cb91c8275d46024f221fae1bad94b6a86c
DD21207 <1005745397@qq.COM>
2018-04-11 20:58:03 +0800

    20180411 21点

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,11 +1,22 @@
 <template>
   <div style="width:100%;height:auto">
-    <ul id="navigator">
-      <li>0</li>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-    </ul>
+    <div class="quadrant_four_scope" id="navigator" style="display: inline-block;" >
+        <a href="javascript:void(0)" id="quadrantB1" class="q_f_item1 q_f_item" quadrant="2"></a>
+        <a href="javascript:void(0)" id="quadrantB2" class="q_f_item2 q_f_item" quadrant="3"></a>
+        <a href="javascript:void(0)" id="quadrantB3" class="q_f_item3 q_f_item" quadrant="0"></a>
+        <a href="javascript:void(0)" id="quadrantB4" class="q_f_item4 q_f_item" quadrant="1"></a>
+    </div>
+    <div class="quadrant_four_scope_tip" id="quadrantScopeTip" style="margin-left: 10px; display: inline-block;">( 提示:点击 <em></em> 选择象限内的关键词 )
+        <!-- ( 提示:点击 <em></em> 选择象限内的关键词 ) -->
+         <div class="view_filter_item" style="margin-top: 10px;">
+                <ul id="quadrantFilterItem">
+                    <li><a class="cur" href="javascript:void(0);" data-type="quadrant.qAvg">平均值</a></li>
+                    <li><a href="javascript:void(0);" data-type="quadrant.qMedian">中位数</a></li>
+                    <!--<li><a href="javascript:void(0);" data-i18n="custom">自定义</a></li>-->
+                </ul>
+                {{plotLineValue.y}}
+        </div>
+    </div>
     <div class="highcharts-container" id="test">
       
     </div>
@@ -32,23 +43,111 @@ export default {
       plotArea : null,
       rect : null,
       plotLineValue : {
-          x: 170,
-          y: 80
+          x: this.options.pointValueX_mean,
+          y: this.options.pointValueY_mean
+      },
+      optionData: {
+        chart: {
+          type: 'scatter',
+          // zoomType:'xy'
+        },
+        title: {
+              text: null
+        },
+        subtitle: {
+          text: null
+        },
+        credits: {
+          enabled: false
+        },
+        xAxis: {
+          lineWidth: 1,
+          title: {
+            text: this.options.titleX,
+            margin:20,
+            style: {
+                    color: 'black',
+                    fontSize:18,
+                }
+          },
+          startOnTick: false, 
+          endOnTick:true,
+          showLastLabel: true,
+          plotLines: [{
+            value: this.options.pointValueX_mean,
+            width: 1,
+            zIndex: 2,
+            color: '#000'
+          }]
+        },
+        yAxis: {
+          lineWidth: 1,
+          gridLineWidth:0,
+          title: {
+            text: this.options.titleY,
+            margin:20,
+            style: {
+                    color: 'black',
+                    fontSize:18,
+                }
+
+          },
+          plotLines: [{
+            value:this.options.pointValueY_mean,
+            width: 1,
+            zIndex: 2,
+            color: '#000'
+          }]
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+          scatter: {
+            marker: {
+              radius: 5,
+              states: {
+                hover: {
+                  enabled: true,
+                  lineColor: 'rgb(100,100,100)'
+                }
+              }
+            },
+            states: {
+              hover: {
+                marker: {
+                  enabled: false
+                }
+              }
+            },
+            tooltip: {
+              headerFormat: '<b>{series.name}</b><br>',
+              pointFormat: '{point.x} cm, {point.y} kg'
+            }
+          }
+        },
+        series: [{
+          name: '女',
+          color: 'rgba(223, 83, 83, .5)',
+          data: this.options.optionData
+        }]
       },
     }
   },
   mounted() {
-    
-       this.initChart();
-   
-   
+    this.$nextTick(function() {
+      this.initChart();
+    });
   },
   methods: {
     initChart() {
+      //获取当前组件
       // console.log(this.$el);
       // this.$el.style.width = (this.styles.width || 800) + 'px';
       // this.$el.style.height = (this.styles.height || 400) + 'px';
-      this.chart = new Highcharts.Chart("test", this.options);
+      this.chart = new Highcharts.Chart("test", this.optionData);
+      // this.chart.series[0].setData(this.options.optionData);
+
 
       var _this= this;
 
@@ -119,10 +218,74 @@ export default {
 
       var nav = document.getElementById('navigator');
       nav.onclick = function(e)  {
-          if(e.target.tagName === 'LI') {
-              _this.goto(e.target.innerText);
+          if(e.target.tagName === 'A') {
+              $('.q_f_item').removeClass("selected");
+              e.target.classList.add("selected");
+              _this.goto(e.target.getAttribute("quadrant"));
           } 
       };
+
+      $(".view_filter_item li a").click(function(){
+
+        $(".view_filter_item li a").removeClass('cur');
+        $(this).addClass('cur');
+        if($(this).attr("data-type") == "quadrant.qAvg"){
+            _this.plotLineValue.x= _this.options.pointValueX_mean;
+            _this.plotLineValue.y= _this.options.pointValueY_mean;
+            $('.q_f_item').removeClass("selected");
+            _this.plotAreas = null;
+            if(_this.rect) {
+              _this.rect.destroy();
+              _this.rect=null;
+            }
+            _this.chart.update({
+               xAxis: {
+                  plotLines: [{
+                    value:_this.options.pointValueX_mean,
+                    width: 1,
+                    zIndex: 2,
+                    color: '#000'
+                  }]
+               },
+               yAxis: {
+                  plotLines: [{
+                  value:_this.options.pointValueY_mean,
+                  width: 1,
+                  zIndex: 2,
+                  color: '#000'
+                }]
+               },
+            })
+        }else if($(this).attr("data-type") == "quadrant.qMedian"){
+            _this.plotLineValue.x= _this.options.pointValueX_median;
+            _this.plotLineValue.y= _this.options.pointValueY_median;
+            $('.q_f_item').removeClass("selected");
+             _this.plotAreas = null;
+            if(_this.rect) {
+              _this.rect.destroy();
+              _this.rect=null;
+            }
+
+            _this.chart.update({
+               xAxis: {
+                  plotLines: [{
+                    value:_this.options.pointValueX_median,
+                    width: 1,
+                    zIndex: 2,
+                    color: '#000'
+                  }]
+               },
+               yAxis: {
+                  plotLines: [{
+                  value:_this.options.pointValueY_median,
+                  width: 1,
+                  zIndex: 2,
+                  color: '#000'
+                }]
+               },
+            })
+        }
+      })
       
       
     },
@@ -180,14 +343,106 @@ export default {
               }
           });
           console.log(points, points.length);
+    },
+    test(){
+      this.chart.update({
+         xAxis: {
+            title: {
+              text: this.options.titleX
+            }
+         },
+         yAxis: {
+            title: {
+              text: this.options.titleY
+            }
+         },
+      })
     }
-  }
+  },
+  watch:{
+        'options.titleX': 'test',
+        'options.titleY': 'test',
+
+  },
 }
 </script>
 
 <style >
   .highcharts-container {
-    width: 800px;
-    height: 400px;
+    width: 100%;
+    height: 500px;
+    margin-top: 10px;
   }
+  .quadrant_four_scope {
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    width: 50px;
+    height: 50px;
+}
+.quadrant_four_scope .q_f_item1 {
+    left: 0;
+    top: 0;
+}
+.quadrant_four_scope .q_f_item2 {
+    right: -1px;
+    top: 0;
+}
+.quadrant_four_scope .q_f_item3 {
+    bottom: -1px;
+    left: 0;
+}
+.quadrant_four_scope .q_f_item4 {
+    bottom: -1px;
+    right: -1px;
+}
+
+.quadrant_four_scope a {
+    display: inline-block;
+    position: absolute;
+    width: 25px;
+    height: 25px;
+    background: #e2e2e2;
+    transition: background 1s;
+    -webkit-transition: background 1s;
+}
+
+.quadrant_four_scope a.selected {
+    background: #FFB2B2;
+}
+
+.quadrant_four_scope_tip {
+    display: inline-block;
+    vertical-align: middle;
+    color: #FFBB78;
+}
+
+.quadrant_four_scope_tip em {
+    display: inline-block;
+    vertical-align: middle;
+    width: 15px;
+    height: 15px;
+    background: #E2E2E2;
+}
+
+ul, ol {
+    list-style-type: none;
+}
+
+.view_filter_item li {
+    display: inline-block;
+    margin-right: 5px;
+}
+
+.view_filter_item li a.cur,.view_filter_item li a:hover {
+    background: #3F89F4;
+    color: #fff;
+    text-decoration: none;
+}
+
+.view_filter_item li a {
+    display: block;
+    padding: 3px 5px;
+    text-decoration: none;
+}
 </style>
